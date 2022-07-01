7b21252951318b442987457755aafdf1f58ed4cf
Jonathan Linnestad <jonathan.linnestad@gjensidige.no>
2022-07-01 04:39:28 +0200

    jowies

--- example/src/main/kotlin/com/jowies/springkurs/SpringKursApplication.kt
+++ example/src/main/kotlin/com/jowies/springkurs/SpringKursApplication.kt
@@ -3,15 +3,12 @@ package com.jowies.springkurs
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties
 class SpringKursApplication
 
 fun main(args: Array<String>) {
-    val app = SpringApplication(SpringKursApplication::class.java)
-    val environment = System.getenv("ENVIRONMENT").lowercase()
-
-    app.setAdditionalProfiles(environment)
-    app.run(*args)
+    runApplication<SpringKursApplication>(*args)
 }
